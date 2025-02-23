965b768e1b6ae6051f4213cb0497171bc0a19e61
willspot <spelumi03@gmail.com>
2025-02-23 14:41:15 -0800

    django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,6 +6,15 @@ from relationship_app import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page for logged-in users
 
+    # URL pattern for LoginView (customized to use your template)
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # URL pattern for LogoutView (customized to use your template)
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # URL pattern for Register (custom registration view)
+    path('register/', register, name='register'),
+
     # URL pattern for the function-based view (FBV)
     path('books/', list_books, name='list_books'),
 
