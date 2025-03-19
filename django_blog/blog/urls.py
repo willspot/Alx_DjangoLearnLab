fb03d1b010ebf64bc9d8f31a402b8ba6692f555d
willspot <spelumi03@gmail.com>
2025-03-19 03:53:45 -0700

    django

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('', views.PostListView.as_view(), name='post-list'),  # List all posts
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),  # Create new post
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),  # View post details
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),  # Edit existing post
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),  # Delete existing post
 ]
