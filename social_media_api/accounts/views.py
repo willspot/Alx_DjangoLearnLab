b494887afe7f19f70888741937be71ad1099562d
willspot <spelumi03@gmail.com>
2025-03-29 11:38:45 -0700

    social media api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,17 +1,23 @@
 from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, TokenSerializer
-from .serializers import UserProfileSerializer  # Import the Profile serializer
 
 # User Registration View
 class RegisterView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        user, token = serializer.save()
+        return Response({
+            'user': UserRegistrationSerializer(user).data,
+            'token': token.key
+        })
 
 # User Login View
 class LoginView(APIView):
@@ -24,12 +30,3 @@ class LoginView(APIView):
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# User Profile View
-class UserProfileView(generics.RetrieveUpdateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user  # Return the current authenticated user
