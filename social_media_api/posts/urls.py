1914b1e7f7c3ab9552205bbdb7de8a9602f5e41b
willspot <spelumi03@gmail.com>
2025-03-29 11:45:45 -0700

    social media api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, PostListView, PostDetailView, CommentListView, CommentDetailView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -9,4 +9,8 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', user_feed, name='user_feed'),
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('comments/', CommentListView.as_view(), name='comment-list'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
 ]
