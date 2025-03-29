66ce3d0ba91e6172ccd1cc6ca52d7db84ac6a987
willspot <spelumi03@gmail.com>
2025-03-29 12:04:11 -0700

    social media api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from .views import FeedView
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, PostListView, PostDetailView, CommentListView, CommentDetailView
 
@@ -8,7 +9,8 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('feed/', user_feed, name='user_feed'),
+    # path('feed/', user_feed, name='user_feed'),
+    path('feed/', FeedView.as_view(), name='feed'), 
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('comments/', CommentListView.as_view(), name='comment-list'),
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -19,6 +20,27 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
+
+    def get_queryset(self):
+        # Get the current authenticated user
+        current_user = self.request.user
+        
+        # Get posts from users that the current user is following
+        followed_users = current_user.following.all()
+        # Add the current user to the queryset so they see their own posts as well
+        followed_users = followed_users | current_user  # Including the current user in the feed
+        
+        # Fetch posts from followed users, ordered by creation date (most recent first)
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 @api_view(['GET'])
